199f818115862d199af5509b41cefe7f02c934cf
Juraj Sadel <juraj.sadel@espressif.com>
2023-07-31 16:07:23 +0200

    Cleanup unwrap() and add a bunch of comments

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,10 @@ use embassy_net::{
     {dns::DnsQueryType, Config, Stack, StackResources},
 };
 use embassy_time::{Duration, Timer};
+use embassy_net::{
+    tcp::TcpSocket,
+    {dns::DnsQueryType, Config, Stack, StackResources},
+};
 
 // Temperature sensor related imports
 use crate::bmp180_async::Bmp180;
